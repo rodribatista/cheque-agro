d64be7ca755d2a62a6b36e1a94285d908021c9f7
rodribatista <rodribatista@icloud.com>
2023-05-29 23:51:51 -0300

    impl gateway

---
+++ api-gateway/src/test/java/com/jmg/checkagro/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmg.checkagro.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
